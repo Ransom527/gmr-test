ab0e559d986ce54d5da0cc84474244c1bbfb00f7
Ransom527 <Ransom527@GMail.com>
2017-07-06 13:36:20 -0600

    Updated directions elements

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import { Map, Marker, InfoWindow } from "google-maps-react";
+import Start from './Start';
+import End from './End';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
+
         <Map google={window.google} zoom={14}>
         <Marker onClick={this.onMarkerClick} name={'Current location'} />
         <InfoWindow onClose={this.onInfoWindowClose}>
           <div>
-          <h1></h1>
+                    <Start />
+          <End />
           </div>
         </InfoWindow>
         </Map>
